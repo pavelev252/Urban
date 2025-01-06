c1b10ae977c167a7b7b69970f1be4842b626a22a
Алексей <pavelev252@gmail.com>
2025-01-06 22:20:44 +0500

    1.01.25 Атрибуты и классы метод __new__

---
+++ Модуль_5/attributes and classes_5_4.py
@@ -0,0 +1,77 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        # Создаём новый объект
+        instance = super().__new__(cls)
+        # Добавляем название здания в историю
+        if args:
+            cls.houses_history.append(args[0])
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, other):
+        self.number_of_floors += other
+        return self
+
+    def __radd__(self, other):
+        self.number_of_floors += other
+        return self
+
+    def __iadd__(self, other):
+        self.number_of_floors += other
+        return self
+
+
+# Пример работы
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
